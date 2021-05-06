b69f65268668d089e684464c94ff0c68630a24ca
fungjoshua <joshua.fung@dashhudson.com>
2021-05-06 12:48:41 -0400

    Minimum Cytoscope frontend functionality

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ require("dotenv").config();
 const cytoscape = require('cytoscape');
 const { getClubhouseEpic } = require('./controllers/clubhouse-controller');
 const app = express();
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  res.send(indexRouter())
+  res.sendFile(__dirname+'/index.html')
 });
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`App listening on port ${process.env.SERVER_PORT}!`)
 });
-
-function indexRouter(){
-  return 'Hello World!';
-}
