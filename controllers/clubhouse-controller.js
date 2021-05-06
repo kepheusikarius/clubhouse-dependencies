000494e103a4862b1c8389dd58a91178764d5977
ConorMcLaughlin <conor@dashhudson.com>
2021-05-06 13:12:09 -0300

    Add logic to parse clubhouse data into cytoscape input

--- controllers/clubhouse-controller.js
+++ controllers/clubhouse-controller.js
@@ -1,22 +1,47 @@
 const axios = require('axios').default;
 
-async function getClubhouseEpic(epicId) {
+function convertStoriesToCytoscapeNodesAndEdges(stories) {
+  console.log(stories);
+  const nodes = [];
+  const edges = [];
+
+  stories.forEach(story => {
+    nodes.push({
+      id: story.id,
+    })
+        
+    if(story.blocked) {
+      story.story_links.forEach(link => {
+        if (link.verb === 'blocks' && link.type === 'object') {
+          edges.push({
+            id: `${link.subject_id}-${story.id}`,
+            source: link.subject_id,
+            target: story.id,
+          })
+        }
+      })
+    }
+  });
+  
+  return {
+    nodes,
+    edges,
+  }
+}
+
+async function getClubhouseEpicStories(epicId) {
   const apiKey = process.env.CLUBHOUSE_API_KEY;
   const clubhouseUrl = process.env.CLUBHOUSE_URL;
 
-  res = await axios.get(`${clubhouseUrl}/epics/${epicId}`, {
+  res = await axios.get(`${clubhouseUrl}/epics/${epicId}/stories`, {
     headers: {
       "Clubhouse-Token": apiKey
     }
   });
-  return res;
-}
-
-function getClubhouseStories() {
-  return 'what a story!'
+  const nodesAndEdges = convertStoriesToCytoscapeNodesAndEdges(res.data);
+  return nodesAndEdges;
 }
 
 module.exports = {
-  getClubhouseEpic,
-  getClubhouseStories,
+  getClubhouseEpicStories,
 }
