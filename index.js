1d81f465129a7f9092adfcc25f4fffbf4378e673
ConorMcLaughlin <conor@dashhudson.com>
2021-05-06 15:41:38 -0300

    Adds ejs, limits ugly code in html

--- index.js
+++ index.js
@@ -1,19 +1,57 @@
 const express = require('express');
 require("dotenv").config();
-const cytoscape = require('cytoscape');
 const { getStoryDependenciesData } = require('./controllers/clubhouse-controller');
 const app = express();
+
 app.use(express.static(__dirname));
 
+app.set('view engine', 'ejs');
+
+const cytoscapeConfig = {
+  layout: {
+    name: 'grid',
+    rows: 1
+  },
+  style: [ // the stylesheet for the graph
+    {
+      selector: 'node',
+      style: {
+        'background-color': '#F00',
+        'label': 'data(id)'
+      }
+    },
+
+    {
+      selector: 'edge',
+      style: {
+        'width': 3,
+        'line-color': '#ccc',
+        'target-arrow-color': '#ccc',
+        'target-arrow-shape': 'triangle',
+        'curve-style': 'bezier'
+      }
+    }
+  ],
+};
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname+'/index.html')
+  res.render('index', {
+    cytoscapeConfig,
+  });
 });
 
 app.get('/epics/:epicId', async (req, res) => {
   const epicId = req.params.epicId;
   const cytoscapeData = await getStoryDependenciesData(epicId);
+
+  const config = {
+    elements: cytoscapeData,
+    ...cytoscapeConfig,
+  }
   
-  res.json({ data: cytoscapeData })
+  res.render('index', {
+    cytoscapeConfig: config,
+  });
 });
 
 app.listen(process.env.SERVER_PORT, () => {
--- utils/cytoscapeUtils.js
+++ utils/cytoscapeUtils.js
@@ -1,19 +1,25 @@
+const { builtinModules } = require("module");
+
 function convertStoriesToCytoscapeNodesAndEdges(stories) {
   const nodes = [];
   const edges = [];
   
   stories.forEach(story => {
     nodes.push({
-      id: story.id,
-    })
+      data: {
+        id: story.id,
+      }
+    });
           
     if(story.blocked) {
       story.story_links.forEach(link => {
         if (link.verb === 'blocks' && link.type === 'object') {
           edges.push({
-            id: `${link.subject_id}-${story.id}`,
-            source: link.subject_id,
-            target: story.id,
+            data: {
+              id: `${link.subject_id}-${story.id}`,
+              source: link.subject_id,
+              target: story.id,
+            }
           })
         }
       })
