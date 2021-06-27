7404ba9664a55e975a4a0bc9cb70eef2e9ef9e8a
Joe Moore <kepheus@gmail.com>
2021-06-27 16:07:27 -0300

    Switch to parseInt

--- src/config.mjs
+++ src/config.mjs
@@ -2,7 +2,7 @@ export let config = {};
 
 export function loadConfig() {
     config = {
-        port: (process.env.SERVER_PORT) ? int(process.env.SERVER_PORT) : 80,
+        port: (process.env.SERVER_PORT) ? parseInt(process.env.SERVER_PORT) : 80,
         ch_url: process.env.CLUBHOUSE_URL ?? 'https://api.clubhouse.io/api',
         ch_api_key: process.env.CLUBHOUSE_TOKEN,
     };
