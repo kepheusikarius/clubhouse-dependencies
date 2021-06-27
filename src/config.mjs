150e020fb5bbf237d476ff0bcff7794ba47a56a4
Joe Moore <kepheus@gmail.com>
2021-06-27 16:12:23 -0300

    Use heroku's port env var

--- src/config.mjs
+++ src/config.mjs
@@ -2,8 +2,8 @@ export let config = {};
 
 export function loadConfig() {
     config = {
-        port: (process.env.SERVER_PORT) ? parseInt(process.env.SERVER_PORT) : 80,
-        ch_url: process.env.CLUBHOUSE_URL ?? 'https://api.clubhouse.io/api',
+        port: process.env.PORT || 3000,
+        ch_url: process.env.CLUBHOUSE_URL || 'https://api.clubhouse.io/api',
         ch_api_key: process.env.CLUBHOUSE_TOKEN,
     };
 };
